77a2658a5187b310ab02a317a6e41f24c5c9cb54
Peter Markou <markoupetr@gmail.com>
2014-07-29 23:49:02 +0300

    Move the JavaScript and Ruby helpers for link_to_add/remove_fields to the plugin.

--- lib/active_form.rb
+++ lib/active_form.rb
@@ -3,6 +3,19 @@ require 'active_form/form'
 require 'active_form/form_collection'
 require 'active_form/form_definition'
 require 'active_form/too_many_records'
+require 'active_form/view_helpers'
 
 module ActiveForm
+  class Engine < ::Rails::Engine
+
+    config.before_initialize do
+      if config.action_view.javascript_expansions
+        config.action_view.javascript_expansions[:link_helpers] = %w(link_helpers)
+      end
+    end
+
+    initializer "active_form.initialize" do |app|
+      ActionView::Base.send :include, ActiveForm::ViewHelpers
+    end
+  end
 end
