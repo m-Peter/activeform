02215e6a9417b26eea75e5b472cf77296d28c3b6
Peter Markou <markoupetr@gmail.com>
2014-07-31 18:35:54 +0300

    Update the ConferencesControllerTest case with tests for dynamically adding/removing Presentations to/from a Speaker.

--- test/dummy/test/controllers/conferences_controller_test.rb
+++ test/dummy/test/controllers/conferences_controller_test.rb
@@ -60,6 +60,51 @@ class ConferencesControllerTest < ActionController::TestCase
     assert_equal "Conference: #{conference_form.name} was successfully created.", flash[:notice]
   end
 
+  test "should create dynamically added presentation to speaker" do
+    assert_difference('Conference.count') do
+      post :create, conference: {
+        name: "Euruco",
+        city: "Athens",
+
+        speaker_attributes: {
+          name: "Petros Markou",
+          occupation: "Developer",
+
+          presentations_attributes: {
+            "0" => { topic: "Ruby OOP", duration: "1h" },
+            "1" => { topic: "Ruby Closures", duration: "1h" },
+            "12312" => { topic: "Ruby Metaprogramming", duration: "2h" }
+          }
+        }
+      }
+    end
+
+    conference_form = assigns(:conference_form)
+
+    assert conference_form.valid?
+    assert_redirected_to conference_path(conference_form)
+
+    assert_equal "Euruco", conference_form.name
+    assert_equal "Athens", conference_form.city
+    
+    assert_equal "Petros Markou", conference_form.speaker.name
+    assert_equal "Developer", conference_form.speaker.occupation
+    
+    assert_equal "Ruby OOP", conference_form.speaker.presentations[0].topic
+    assert_equal "1h", conference_form.speaker.presentations[0].duration
+    assert_equal "Ruby Closures", conference_form.speaker.presentations[1].topic
+    assert_equal "1h", conference_form.speaker.presentations[1].duration
+    assert_equal "Ruby Metaprogramming", conference_form.speaker.presentations[2].topic
+    assert_equal "2h", conference_form.speaker.presentations[2].duration
+
+    assert conference_form.speaker.persisted?
+    conference_form.speaker.presentations.each do |presentation|
+      presentation.persisted?
+    end
+    
+    assert_equal "Conference: #{conference_form.name} was successfully created.", flash[:notice]
+  end
+
   test "should not create conference with invalid params" do
     conference = conferences(:ruby)
 
@@ -135,6 +180,40 @@ class ConferencesControllerTest < ActionController::TestCase
     assert_equal "1h", conference_form.speaker.presentations[0].duration
     assert_equal "Rails OOP", conference_form.speaker.presentations[1].topic
     assert_equal "1h", conference_form.speaker.presentations[1].duration
+    
+    assert_equal "Conference: #{conference_form.name} was successfully updated.", flash[:notice]
+  end
+
+  test "should destroy dynamically removed presentation from speaker" do
+    assert_difference('Conference.count', 0) do
+      patch :update, id: @conference, conference: {
+        name: "GoGaruco",
+        city: "Golden State",
+
+        speaker_attributes: {
+          name: "John Doe",
+          occupation: "Developer",
+
+          presentations_attributes: {
+            "0" => { topic: "Rails OOP", duration: "1h", id: presentations(:ruby_oop).id },
+            "1" => { topic: "Rails Patterns", duration: "1h", id: presentations(:ruby_closures).id, _destroy: "1" },
+          }
+        }
+      }
+    end
+
+    conference_form = assigns(:conference_form)
+
+    assert_redirected_to conference_path(conference_form)
+    
+    assert_equal "GoGaruco", conference_form.name
+    assert_equal "Golden State", conference_form.city
+    
+    assert_equal "John Doe", conference_form.speaker.name
+    assert_equal "Developer", conference_form.speaker.occupation
+    
+    assert_equal "Rails OOP", conference_form.speaker.presentations[0].topic
+    assert_equal "1h", conference_form.speaker.presentations[0].duration
     
     assert_equal "Conference: #{conference_form.name} was successfully updated.", flash[:notice]
   end
