5cf503f8b12410805af0a0d7fde64f4df942602d
Peter Markou <markoupetr@gmail.com>
2014-08-01 11:34:12 +0300

    Update the dummy app to use the plugin's helpers for its scaffolds.

--- test/dummy/app/assets/javascripts/application.js
+++ test/dummy/app/assets/javascripts/application.js
@@ -10,32 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery_ujs
+//= require link_helpers
 //= require_tree .
-
-(function($) {
-  $(document).on('click', '.remove_fields.dynamic, .remove_fields.existing', function(e) {
-    var $this = $(this), 
-        node_to_delete = $this.closest('.nested-fields');
-
-    e.preventDefault();
-
-    if ($this.hasClass('dynamic')) {
-        node_to_delete.remove();
-    } else {
-        $this.prev("input[type=hidden]").val("1");
-        node_to_delete.hide();
-    }
-  });
-
-  $(document).on('click', '.add_fields', function(e) {
-    e.preventDefault();
-    
-    var $this = $(this),
-      association = $this.data('association'),
-      content = $this.data('association-insertion-template');
-
-    var new_id = new Date().getTime();
-    var regex = new RegExp("new_" + association, "g");
-    $this.before(content.replace(regex, new_id));
-  });
-})(jQuery);
