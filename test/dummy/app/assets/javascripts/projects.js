ee886ad4ee7b9169bb10105c6addee7bce36cec5
Peter Markou <markoupetr@gmail.com>
2014-08-06 22:18:36 +0300

    Add some effects for the Owner and Tag associations.

--- test/dummy/app/assets/javascripts/projects.js
+++ test/dummy/app/assets/javascripts/projects.js
@@ -1,2 +1,47 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).ready(function() {
+    $("#owner a.add_fields").
+      data("association-insertion-position", 'before').
+      data("association-insertion-node", 'this');
+
+    $('#owner').bind('cocoon:after-insert',
+         function() {
+           $("#owner_from_list").hide();
+           $("#owner a.add_fields").hide();
+         });
+    $('#owner').bind("cocoon:after-remove",
+         function() {
+           $("#owner_from_list").show();
+           $("#owner a.add_fields").show();
+         });
+
+    $("#tags a.add_fields").
+      data("association-insertion-position", 'before').
+      data("association-insertion-node", 'this');
+
+    $('#tags').bind('cocoon:after-insert',
+         function(e, tag) {
+             $(".project-tag-fields a.add_fields").
+                 data("association-insertion-position", 'before').
+                 data("association-insertion-node", 'this');
+             $('.project-tag-fields').bind('cocoon:after-insert',
+                  function() {
+                    $(this).children("#tag_from_list").remove();
+                    $(this).children("a.add_fields").hide();
+                  });
+         });
+
+    $('#tasks').bind('cocoon:before-insert', function(e,task_to_be_added) {
+        task_to_be_added.fadeIn('slow');
+    });
+
+    $('#tasks').bind('cocoon:after-insert', function(e, added_task) {
+        //added_task.css("background","red");
+    });
+
+    $('#tasks').bind('cocoon:before-remove', function(e, task) {
+        $(this).data('remove-timeout', 1000);
+        task.fadeOut('slow');
+    })
+
+    $('body').tabs();
+});
