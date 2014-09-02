0badbdd608e7adf330b379c2287603c2847f62b8
Peter Markou <markoupetr@gmail.com>
2014-09-02 17:45:04 +0300

    The view_helpers.rb and link_helpers.js files contain only required code. Code for functionality that isn't exercised is removed.

--- lib/active_form/view_helpers.rb
+++ lib/active_form/view_helpers.rb
@@ -21,7 +21,14 @@ module ActiveForm
 
     def render_association(association, f, new_object, render_options={}, custom_partial=nil)
       partial = get_partial_path(custom_partial, association)
-      method_name = f.respond_to?(:semantic_fields_for) ? :semantic_fields_for : (f.respond_to?(:simple_fields_for) ? :simple_fields_for : :fields_for)
+      
+      if f.respond_to?(:semantic_fields_for)
+        method_name = :semantic_fields_for
+      elsif f.respond_to?(:simple_fields_for)
+        method_name = :simple_fields_for
+      else
+        method_name = :fields_for
+      end
       
       f.send(method_name, association, new_object, {:child_index => "new_#{association}"}.merge(render_options)) do |builder|
         render(partial: partial, locals: {:f => builder})
@@ -34,8 +41,7 @@ module ActiveForm
       override_partial = html_options.delete(:partial)
 
       html_options[:class] = [html_options[:class], "add_fields"].compact.join(' ')
-      html_options[:'data-association'] = association.to_s.singularize
-      html_options[:'data-associations'] = association.to_s.pluralize
+      html_options[:'data-association'] = association.to_s
 
       new_object = create_object(f, association)
 
