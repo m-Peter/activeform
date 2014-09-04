2044f3c367f9070891755d51bd34f96994cc575e
Peter Markou <markoupetr@gmail.com>
2014-09-04 13:52:04 +0300

    Add an install generator to create a forms directory into the app and test directories and include the JS file.

---
+++ lib/generators/install_generator.rb
@@ -0,0 +1,26 @@
+require 'rails/generators/base'
+
+module ActiveForm
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+
+      desc "Creates a forms directory into your app and test directories and includes the necessary JS file."
+
+      def create_forms_app_directory
+        empty_directory "app/forms"
+      end
+
+      def create_forms_test_directory
+        if File.directory?("spec")
+          empty_directory "spec/forms"
+        else
+          empty_directory "test/forms"
+        end
+      end
+
+      def include_js_file
+        insert_into_file "app/assets/javascripts/application.js", "//= require link_helpers", :before => "//= require_tree ."
+      end
+    end
+  end
+end
